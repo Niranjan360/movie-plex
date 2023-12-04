5a27674d4969292a651d517327898e212213b5ea
Niranjan360 <niranjanniru360@gmail.com>
2023-12-04 15:30:41 +0530

    sliderfy comp added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,41 @@
 import { useEffect, useState } from 'react';
 import '../styles/home.css'
 import Movielist from './Movielist';
+import { Sliderify } from "react-sliderify";
+
 
 const Home = () => {
 
     let[movies , setMovies] = useState(null);
+    let[sliderMovies , setsliderMovies] = useState(null);
 
     useEffect(()=>{
         fetch("http://localhost:4000/movies")
         .then(res=>res.json())
-        .then((data)=>{setMovies(data);})
+        .then((data)=>{
+            setMovies(data);
+            setsliderMovies(data.slice(0,5))
+        })
     } , [])
 
 
     return ( <div className="home-comp">
 
+           {sliderMovies && 
+           <Sliderify>
+            {
+                sliderMovies.map((m)=>{ return(
+                                                <div style={{ height: "500px", background: `url(${m.banner})`, backgroundRepeat: "no-repeat",backgroundSize:"cover" }}>
+                                                </div>
+                )})
+            }
+            </Sliderify>}
+
                {movies && <Movielist movies={movies} title="All movies"/>}
 
-               {movies && <Movielist movies={movies.filter((v)=>{return v.release_date<2000})} title="90's movies"/>}
+               {movies && <Movielist movies={movies.filter((m)=>{return m.languages.includes("English")})} title="Hollywood movies"/>}
+
+               {movies && <Movielist movies={movies.filter((m)=>{return m.rating>=9})} title="Top rated movies"/>}
 
 
             </div> );
