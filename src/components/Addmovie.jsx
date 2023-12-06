d6a5c315493a3c7405e7aea9a9a953727fd744c2
Niranjan360 <niranjanniru360@gmail.com>
2023-12-06 16:14:39 +0530

    add movie comp code

--- src/components/Addmovie.jsx
+++ src/components/Addmovie.jsx
@@ -1,8 +1,85 @@
 const Addmovie = () => {
+
+    let yearsArray = [];
+    for (let year = 1950; year <= 2023; year++) {
+        yearsArray.push(year);
+    }
     
-    return ( <div className="addmovie-comp">
-                <h1>This is Addmovie comp</h1>
-            </div> );
+
+    
+    return ( 
+    <div className="addmovie-comp">
+        <h1>Add New Movie</h1>
+
+        <form>  
+            <input type="text" placeholder="Movie Name" />
+            <input type="text" placeholder="Hero Name" />
+            <input type="text" placeholder="Heroine Name" />
+            <input type="text" placeholder="Director" />
+            <input type="text" placeholder="Production house" />
+            <input type="text" placeholder="Budget in crores" />
+            <input type="text" placeholder="collection in crores" />
+            <fieldset>
+                <legend>Genre</legend>
+                <div>
+                    <input type="checkbox" name="genre" id="ac" /> <label htmlFor="ac">Action</label>
+                    <input type="checkbox" name="genre" id="ad" /> <label htmlFor="ad">Adventure</label>
+                    <input type="checkbox" name="genre" id="cr" /> <label htmlFor="cr">Crime</label>
+                    <input type="checkbox" name="genre" id="co" /> <label htmlFor="co">Comedy</label>
+                    <input type="checkbox" name="genre" id="ho" /> <label htmlFor="ho">Horror</label>
+                    <input type="checkbox" name="genre" id="ro" /> <label htmlFor="ro">Romantic</label>
+                    <input type="checkbox" name="genre" id="th" /> <label htmlFor="th">Thriller</label>
+                    <input type="checkbox" name="genre" id="sf" /> <label htmlFor="sf">Sci-fi</label>
+                </div>
+            </fieldset>
+            <select>
+                <option selected disabled>Released Year</option>
+                {
+                    yearsArray.map((y)=>{return(<option>{y}</option>)})
+                }
+            </select>
+            <input type="number" placeholder="Rating" min="1" max="10" step="0.1"/>
+            <textarea placeholder="Movie Description" cols="30" rows="10"></textarea>
+            <div className="duration">
+                <select>
+                    <option selected disabled>Hours</option>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+                <select>
+                    <option selected disabled>Minutes</option>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>5</option>
+                    <option>....</option>
+                </select>
+            </div>
+            <fieldset>
+                <legend>Languages</legend>
+                <div>
+                    <input type="checkbox" name="lang" id="eb" /> <label htmlFor="ac">English</label>
+                    <input type="checkbox" name="lang" id="ad" /> <label htmlFor="ad">Hindi</label>
+                    <input type="checkbox" name="lang" id="cr" /> <label htmlFor="cr">Kannada</label>
+                    <input type="checkbox" name="lang" id="co" /> <label htmlFor="co">Tamil</label>
+                    <input type="checkbox" name="lang" id="ho" /> <label htmlFor="ho">Telgu</label>
+                    <input type="checkbox" name="lang" id="ro" /> <label htmlFor="ro">Malayalam</label>
+                </div>
+            </fieldset>
+            <input type="url" placeholder="trailer_link" />
+            <input type="url" placeholder="banner" />
+            <input type="url" placeholder="poster" />
+
+            <input type="submit" value="Add new movie" />
+        </form>
+    </div> );
 }
  
 export default Addmovie;
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,20 +1,60 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ScaleLoader from 'react-spinners/ScaleLoader'
+import '../styles/description.css'
+import Movielist from "./Movielist";
 
 const Description = () => {
 
     let {id} = useParams();
     let[movie , setMovie] = useState(null);
+    let[movies , setMovies] = useState(null);
+
 
     useEffect(()=>{
         fetch("http://localhost:4000/movies/" + id)
         .then(res=>res.json())
-        .then((data)=>{console.log(data);})
+        .then((data)=>{
+            setMovie(data)
+            console.log(data);
+        })
+
+        fetch("http://localhost:4000/movies")
+        .then(res=>res.json())
+        .then((data)=>{
+            setMovies(data)
+            console.log(data);
+        })
     } , [])
 
     
     return ( <div className="description-comp">
-                <h1>This is Description comp {id} </h1>
+
+            {movie==null && <ScaleLoader size={450}/>}
+
+            {movie &&  
+            <>
+                <div className="movie-description" style={{background : `url(${movie.banner})`,backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
+                                <div className="container">
+                                    <img src={movie.poster} alt="" />
+                                    <div className="details">
+                                        <h1>{movie.moviename}</h1>
+                                        <span>{movie.languages.join("  ,  ")}</span>
+                                        <span>{movie.genre}</span>
+                                        <span>{movie.rating} 🌟  </span>
+                                        <p>{movie.description}</p>
+                                    </div>
+                                </div>
+                </div> 
+
+                <div className="trailer">
+                    <iframe width="560" height="315" src={movie.trailer_link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                {movies && <Movielist movies={movies.filter((m)=>{ return m.genre.includes(movie.genre)})} title="Similar Movies"/>}
+            </>         
+            }
+
             </div> );
 }
  
