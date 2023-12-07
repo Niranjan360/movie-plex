8b76944f47062224e7274c80ccefc2f294cdb3ba
Niranjan360 <niranjanniru360@gmail.com>
2023-12-07 15:43:29 +0530

    adding new movie

--- src/components/Addmovie.jsx
+++ src/components/Addmovie.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import '../styles/addmovie.css'
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const Addmovie = () => {
 
@@ -8,125 +12,166 @@ const Addmovie = () => {
     for (let year = 1950; year <= 2023; year++) {
         yearsArray.push(year);
     }
+    
+    let navigate = useNavigate();
 
     let[movie , setmovie] = useState("");
     let[hero , setHero] = useState("");
+    let[heroine , setHeroine] = useState("");
+    let[director , setDirector] = useState("");
+    let[pd , setPd] = useState("");
+    let[budget , setBudget] = useState("");
+    let[collection , setCollection] = useState("");
+    let[rating , setRating] = useState(0);
+    let[description , setDescription] = useState("");
+    let[yor , setYor] = useState("");
+    let[hours , setHours] = useState(0);
+    let[minutes , setMinutes] = useState(0);
+    // let[genre , setGenre] = useState([]);
+    // let[languages , setLanguages] = useState([]);
+    let[trailerLink , setTrailerLink] = useState("");
     let[poster , setPoster] = useState("");
-    let navigate = useNavigate();
+    let[banner , setBanner] = useState("");
 
-    let addMovie = (e)=>{
+
+    let handlAddMovie = (e)=>{
+        // stop the default actions done by subbmission event
         e.preventDefault();
 
-        let newMovie = {
-            moviename : movie,
-            hero : hero ,
-            poster : poster,
-            heroine:"Shree nidhi",
-            director:"Prashanth neel",
-            production_house:"Hombale",
-            budget:"100cr",
-            collection:"800cr",
-            genre:"Action drama",
-            release_date: "2019",
-            rating:"9.1",
-            description:"KGF is an Indian period action film series set mostly in the Kolar Gold Fields, which gives the series its name, created by Prashanth Neel and produced by Hombale Films starring Yash in lead role with an ensemble supporting cast.[4] Set in the late 1970s and early 1980s, the series follows two narrators, Anand Ingalangi and his son Vijayendra Ingalangi, who gives an interview of a book written by Anand to a leading news channel, which tells the life story of Raja Krishnappa Bairya alias Rocky (Yash), a Mumbai-based high ranking mercenary born in poverty and how he became the most dreaded person at that time.",
-            duration:"2h 45mins",
-            languages:["Kannada","Hindi","Tamil","Telgu","English"],
-            trailer_link:"https://www.youtube.com/embed/-KfsY-qwBS0?si=dHakex7DJtzFxR28",
-            banner:"https://www.koimoi.com/wp-content/new-galleries/2022/04/kgf-sets-foot-in-metaverse-fans-snap-up-nfts-in-record-time-001.jpg",
+        // prepare the data
+        let newMovie  = {
+                            "moviename": movie,
+                            "hero": hero,
+                            "heroine": heroine,
+                            "director": director,
+                            "production_house": pd,
+                            "budget": budget+"cr",
+                            "collection": collection+"cr",
+                            "genre": [],
+                            "release_date": yor,
+                            "rating": rating,
+                            "description": description,
+                            "duration": `${hours}hrs ${minutes}min`,
+                            "languages": [],
+                            "trailer_link": trailerLink,
+                            "banner": banner,
+                            "poster": poster
+                        }
+
+        let goptions = document.getElementsByName("genre");
+        for (let i = 0; i < goptions.length; i++) 
+        {
+            if( goptions[i].checked)
+            {
+                newMovie.genre.push(goptions[i].value);
+            }
         }
 
-        let config = {
-                        method : "POST",
-                        headers: {"Content-Type":"application/json"},
-                        body :JSON.stringify(newMovie)
-                     }
+        let langOptions = document.getElementsByName("lang");
+        for (let i = 0; i < langOptions.length; i++) 
+        {
+            if( langOptions[i].checked)
+            {
+                newMovie.languages.push(langOptions[i].value);
+            }
+        }
 
-        fetch("http://localhost:4000/movies" , config)
+    
+        // post the data to db
+        fetch("http://localhost:4000/movies" , 
+                                                {
+                                                    method : "POST",
+                                                    headers : {"Content-Type":"application/json"},
+                                                    body : JSON.stringify(newMovie)
+                                                })
         .then(()=>{
-            alert("new movie added");
-            // window.location.reload();
-            // use react tostify package
-            navigate("/")
-        }) 
+            toast.success("Movie added to data successfuly");
+            setTimeout(()=>{ navigate("/")}, 2000);
+        })
     }
+
 
     
     return ( 
     <div className="addmovie-comp">
         <h1>Add New Movie</h1>
 
-        <form onSubmit={addMovie}>  
-            <input type="text" placeholder="Movie Name" value={movie} onChange={(e)=>{setmovie(e.target.value);}}/>
-            <input type="text" placeholder="Hero Name" value={hero} onChange={(e)=>{setHero(e.target.value);}}/>
-
-
-
-            <input type="text" placeholder="Heroine Name" />
-            <input type="text" placeholder="Director" />
-            <input type="text" placeholder="Production house" />
-            <input type="text" placeholder="Budget in crores" />
-            <input type="text" placeholder="collection in crores" />
-            <fieldset>
-                <legend>Genre</legend>
-                <div>
-                    <input type="checkbox" name="genre" id="ac" /> <label htmlFor="ac">Action</label>
-                    <input type="checkbox" name="genre" id="ad" /> <label htmlFor="ad">Adventure</label>
-                    <input type="checkbox" name="genre" id="cr" /> <label htmlFor="cr">Crime</label>
-                    <input type="checkbox" name="genre" id="co" /> <label htmlFor="co">Comedy</label>
-                    <input type="checkbox" name="genre" id="ho" /> <label htmlFor="ho">Horror</label>
-                    <input type="checkbox" name="genre" id="ro" /> <label htmlFor="ro">Romantic</label>
-                    <input type="checkbox" name="genre" id="th" /> <label htmlFor="th">Thriller</label>
-                    <input type="checkbox" name="genre" id="sf" /> <label htmlFor="sf">Sci-fi</label>
-                </div>
-            </fieldset>
-            <select>
-                <option selected disabled>Released Year</option>
-                {
-                    yearsArray.map((y)=>{return(<option>{y}</option>)})
-                }
-            </select>
-            <input type="number" placeholder="Rating" min="1" max="10" step="0.1"/>
-            <textarea placeholder="Movie Description" cols="90" rows="5"></textarea>
-            <div className="duration">
-                <select>
-                    <option selected disabled>Hours</option>
-                    <option>0</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-                <select>
-                    <option selected disabled>Minutes</option>
-                    <option>0</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>5</option>
-                    <option>....</option>
-                </select>
+        <form onSubmit={handlAddMovie}>  
+            <div className='cl1'>
+                <input type="text" placeholder="Movie Name" value={movie} onChange={(e)=>{setmovie(e.target.value);}}/>
+                <input type="text" placeholder="Hero Name" value={hero} onChange={(e)=>{setHero(e.target.value);}}/>
+                <input type="text" placeholder="Heroine Name" value={heroine} onChange={(e)=>{setHeroine(e.target.value);}} />
+                <input type="text" placeholder="Director" value={director} onChange={(e)=>{setDirector(e.target.value);}}/>
+                <input type="text" placeholder="Production house" value={pd} onChange={(e)=>{setPd(e.target.value);}}/>
+                <input type="text" placeholder="Budget in crores" value={budget} onChange={(e)=>{setBudget(e.target.value);}} />
+                <input type="text" placeholder="collection in crores" value={collection} onChange={(e)=>{setCollection(e.target.value);}}/>
+                <fieldset>
+                    <legend align="center">Genre</legend>
+                    <div>
+                        <input type="checkbox" name="genre" id="ac" value="Action" /> <label htmlFor="ac">Action</label>
+                        <input type="checkbox" name="genre" id="ad" value="Adventure"/> <label htmlFor="ad">Adventure</label>
+                        <input type="checkbox" name="genre" id="cr" value="Crime"/> <label htmlFor="cr">Crime</label>
+                        <input type="checkbox" name="genre" id="co" value="Comedy"/> <label htmlFor="co">Comedy</label>
+                        <input type="checkbox" name="genre" id="ho" value="Horror"/> <label htmlFor="ho">Horror</label>
+                        <input type="checkbox" name="genre" id="ro" value="Romantic"/> <label htmlFor="ro">Romantic</label>
+                        <input type="checkbox" name="genre" id="th" value="Thriller"/> <label htmlFor="th">Thriller</label>
+                        <input type="checkbox" name="genre" id="sf" value="Sci-fi"/> <label htmlFor="sf">Sci-fi</label>
+                    </div>
+                </fieldset>
             </div>
-            <fieldset>
-                <legend>Languages</legend>
-                <div>
-                    <input type="checkbox" name="lang" id="eb" /> <label htmlFor="ac">English</label>
-                    <input type="checkbox" name="lang" id="ad" /> <label htmlFor="ad">Hindi</label>
-                    <input type="checkbox" name="lang" id="cr" /> <label htmlFor="cr">Kannada</label>
-                    <input type="checkbox" name="lang" id="co" /> <label htmlFor="co">Tamil</label>
-                    <input type="checkbox" name="lang" id="ho" /> <label htmlFor="ho">Telgu</label>
-                    <input type="checkbox" name="lang" id="ro" /> <label htmlFor="ro">Malayalam</label>
+            <div className="cl2">
+                <div className="yor">
+                    <select onChange={(e)=>{setYor(e.target.value);}}>
+                        <option selected disabled>Released Year</option>
+                        {
+                            yearsArray.map((y)=>{return(<option value={y}>{y}</option>)})
+                        }
+                    </select>
                 </div>
-            </fieldset>
-            <input type="url" placeholder="trailer_link" />
-            <input type="url" placeholder="banner" />
-            <input type="url" placeholder="poster" value={poster} onChange={(e)=>{setPoster(e.target.value)}}/>
+                <input type="number" placeholder="Rating" min="1" max="10" step="0.1"  value={rating} onChange={(e)=>{setRating(e.target.value);}}/>
+                <textarea placeholder="Movie Description" cols="90" rows="5"  value={description} onChange={(e)=>{setDescription(e.target.value);}}></textarea>
+                <div className="duration">
+                    <select onChange={(e)=>{setHours(e.target.value)}}>
+                        <option selected disabled>Hours</option>
+                        <option>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    <select onChange={(e)=>{setMinutes(e.target.value)}}>
+                        <option selected disabled>Minutes</option>
+                        <option>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>5</option>
+                        <option>....</option>
+                    </select>
+                </div>
+                <fieldset>
+                    <legend align="center">Languages</legend>
+                    <div>
+                        <input type="checkbox" name="lang" id="eng" value="English"/> <label htmlFor="eng">English</label>
+                        <input type="checkbox" name="lang" id="hin" value="Hindi"/> <label htmlFor="hin">Hindi</label>
+                        <input type="checkbox" name="lang" id="kn" value="Kannada"/> <label htmlFor="kn">Kannada</label>
+                        <input type="checkbox" name="lang" id="tn" value="Tamil"/> <label htmlFor="tn">Tamil</label>
+                        <input type="checkbox" name="lang" id="tl" value="Telgu"/> <label htmlFor="tl">Telgu</label>
+                        <input type="checkbox" name="lang" id="ml" value="Malayalam"/> <label htmlFor="ml">Malayalam</label>
+                    </div>
+                </fieldset>
+                <input type="url" placeholder="trailer_link" value={trailerLink} onChange={(e)=>{setTrailerLink(e.target.value)}}/>
+                <input type="url" placeholder="banner" value={banner} onChange={(e)=>{setBanner(e.target.value)}}/>
+                <input type="url" placeholder="poster" value={poster} onChange={(e)=>{setPoster(e.target.value)}}/>
 
-            <input type="submit" value="Add new movie" />
+                <input type="submit" value="Add new movie" />
+            </div>
         </form>
+
+        <ToastContainer />
     </div> );
 }
  
